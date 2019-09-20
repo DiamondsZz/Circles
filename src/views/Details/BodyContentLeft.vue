0d327dcbfa49df0192287502860fae844c75d593
DiamondsZz <951311326@qq.com>
2019-09-20 17:07:29 +0800

    detail body

---
+++ src/views/Details/BodyContentLeft.vue
@@ -0,0 +1,205 @@
+<!-- 文章详情内容身体     左-->
+<template>
+  <div class="left">
+    <div class="left-head">
+      <span class="left-head-left">133个回答</span>
+      <div class="left-head-right">
+        默认排序
+        <a-icon class="left-head-right-icon" type="menu-fold" />
+      </div>
+    </div>
+    <div class="left-body">
+      <div class="left-body-item">
+        <div class="left-body-user">
+          <span class="left-body-user-img">
+            <img src="https://pic3.zhimg.com/v2-c5f36eeb6a86c774a0b8a6d1e5128334_xs.jpg" alt />
+          </span>
+          <div class="left-body-user-text">
+            <div class="left-body-user-name">Shawn Tsien</div>
+            <div class="left-body-user-detail">喜欢研究的商人（民科）</div>
+          </div>
+        </div>
+        <div class="left-body-person">111人赞同了该回答</div>
+        <div class="left-body-content">
+          你举的例子，依然都还是扯谈，虽然似乎呈现在眼前了，
+          但实际与当初吹嘘的效果差别很大。
+          我已经得出一条屡试不爽的规律：
+          凡是让我惊讶的新技术、新发现，都是吹牛，
+          尤其是出现在中国的。如果文章的标题有“美国人慌了”、
+          “全世界震惊”、“突然宣布”、“懵了”等字眼，
+          这些文章根本不用点开。
+        </div>
+        <div class="left-body-expand">
+          展开阅读全文
+          <a-icon type="down" />
+        </div>
+        <div class="left-body-time">发布于昨天 22:20</div>
+        <div class="left-body-actions">
+          <span class="left-body-actions-item">
+            <a-button class="left-body-actions-up" icon="caret-up">赞同 4</a-button>
+            <a-button class="left-body-actions-down" icon="caret-down"></a-button>
+          </span>
+          <span class="left-body-actions-item left-body-actions-text">
+            <a-icon class="left-body-actions-icon" type="message" />24 条评论
+          </span>
+          <span class="left-body-actions-item left-body-actions-text">
+            <a-icon class="left-body-actions-icon" type="rocket" />分享
+          </span>
+          <span class="left-body-actions-item left-body-actions-text">
+            <a-icon class="left-body-actions-icon" type="star" />收藏
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+  created() {},
+  components: {}
+};
+</script>
+<style scoped>
+.left {
+  width: 694px;
+  background-color: #fff;
+  margin-right: 10px;
+  flex-shrink: 0;
+}
+
+/*头部*/
+.left .left-head {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 50px;
+  margin: 0 20px;
+  border-bottom: 1px solid #f6f6f6;
+}
+.left-head .left-head-left {
+  font-size: 15px;
+  font-weight: 600;
+}
+.left-head .left-head-right {
+  color: #8590a6;
+  margin-right: 10px;
+  cursor: pointer;
+}
+.left-head-right-icon {
+  margin-left: 10px;
+}
+/*身体*/
+.left-body-item {
+  padding: 16px 20px;
+}
+.left-body-item::after {
+  display: inline-block;
+  width: 100%;
+  content: "";
+  border-bottom: 1px solid #f6f6f6;
+}
+.left-body .left-body-user {
+  display: flex;
+  align-items: center;
+}
+.left-body-user .left-body-user-img img {
+  width: 38px;
+  height: 38px;
+}
+.left-body-user .left-body-user-text {
+  margin-left: 14px;
+}
+.left-body-user-text .left-body-user-name {
+  font-size: 15px;
+  font-weight: 600;
+}
+.left-body-user-text .left-body-user-detail {
+  width: 500px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  color: #646464;
+}
+.left-body .left-body-person {
+  color: #8590a6;
+  margin: 10px 0;
+}
+.left-body .left-body-content {
+  line-height: 24px;
+
+  /* 遮罩层 */
+  position: relative;
+  -webkit-transition: max-height 0.32s cubic-bezier(0.95, 0.05, 0.795, 0.035),
+    -webkit-mask-size 0.22s cubic-bezier(0.95, 0.05, 0.795, 0.035);
+  transition: max-height 0.32s cubic-bezier(0.95, 0.05, 0.795, 0.035),
+    -webkit-mask-size 0.22s cubic-bezier(0.95, 0.05, 0.795, 0.035);
+  transition: mask-size 0.22s cubic-bezier(0.95, 0.05, 0.795, 0.035),
+    max-height 0.32s cubic-bezier(0.95, 0.05, 0.795, 0.035);
+  transition: mask-size 0.22s cubic-bezier(0.95, 0.05, 0.795, 0.035),
+    max-height 0.32s cubic-bezier(0.95, 0.05, 0.795, 0.035),
+    -webkit-mask-size 0.22s cubic-bezier(0.95, 0.05, 0.795, 0.035);
+  -webkit-mask-image: -webkit-gradient(
+    linear,
+    left top,
+    left bottom,
+    from(#1a1a1a),
+    to(transparent)
+  );
+  -webkit-mask-image: linear-gradient(
+    #1a1a1a calc(100% - 8rem),
+    transparent calc(100% - 2.8rem)
+  );
+  mask-image: -webkit-gradient(
+    linear,
+    left top,
+    left bottom,
+    from(#1a1a1a),
+    to(transparent)
+  );
+  mask-image: linear-gradient(
+    #1a1a1a calc(100% - 8rem),
+    transparent calc(100% - 2.8rem)
+  );
+  -webkit-mask-size: 100% 100%;
+  mask-size: 100% 100%;
+}
+.left-body .left-body-expand {
+  color: #175199;
+  font-size: 15px;
+  text-align: center;
+  cursor: pointer;
+}
+.left-body .left-body-time {
+  color: #8590a6;
+  margin: 10px 0;
+}
+
+.left-body-actions .left-body-actions-up {
+  margin-right: 5px;
+}
+.left-body-actions .left-body-actions-up {
+  color: #0084ff;
+  background-color: rgba(0, 132, 255, 0.1) !important;
+  border: none;
+}
+.left-body-actions .left-body-actions-down {
+  color: #0084ff;
+  background-color: rgba(0, 132, 255, 0.1) !important;
+  border: none;
+}
+.left-body .left-body-actions .left-body-actions-item {
+  margin-right: 20px;
+}
+.left-body .left-body-actions .left-body-actions-icon {
+  margin-right: 6px;
+}
+.left-body .left-body-actions .left-body-actions-text {
+  color: #8590a6;
+  cursor: pointer;
+}
+</style>
---
+++ src/views/Details/BodyContentRight.vue
@@ -0,0 +1,134 @@
+<!-- 文章详情内容身体   右-->
+<template>
+  <div class="right">
+    <div class="right-question">
+      <div class="right-question-head">相关问题</div>
+      <div class="right-question-body">
+        <div class="right-question-body-item">
+          5G的普及会对无人驾驶带来改变么？
+          <span class="right-question-body-answer">3个回答</span>
+        </div>
+      </div>
+    </div>
+    <div class="right-recommend">
+      <div class="right-recommend-head">相关推荐</div>
+      <div class="right-recommend-body">
+        <div class="right-recommend-body-item">
+          <div class="right-recommend-body-img">
+            <img src="https://pic4.zhimg.com/90/v2-6956ae2f9652788ac6a67529b8d515b5_250x0.jpg" alt />
+          </div>
+          <div class="right-recommend-body-inf">
+            <div class="right-recommend-body-top">千年雄安：颠覆性技术、前沿产业与未来之都</div>
+            <div class="right-recommend-body-bottom">
+              <span class="right-recommend-body-bottom-person">29 人读过</span>
+              <span class="right-recommend-body-bottom-read">
+                <a-button icon="caret-up">阅读</a-button>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+  created() {},
+  components: {}
+};
+</script>
+<style scoped>
+.right {
+  width: 296px;
+}
+/*相关问题*/
+.right-question {
+  margin-bottom: 10px;
+  background: #fff;
+  overflow: hidden;
+  border-radius: 2px;
+  box-shadow: 0 1px 3px rgba(26, 26, 26, 0.1);
+}
+.right-question .right-question-head {
+  height: 50px;
+  line-height: 50px;
+  padding: 0 20px;
+  font-weight: 600;
+  border-bottom: 1px solid #f6f6f6;
+}
+.right-question .right-question-body {
+  padding: 16px 20px 0px;
+}
+.right-question .right-question-body .right-question-body-item {
+  margin-bottom: 16px;
+  color: #175199;
+}
+.right-question-body .right-question-body-item .right-question-body-answer {
+  color: #8590a6;
+}
+
+/*相关推荐*/
+.right-recommend {
+  margin-bottom: 10px;
+  background: #fff;
+  overflow: hidden;
+  border-radius: 2px;
+  box-shadow: 0 1px 3px rgba(26, 26, 26, 0.1);
+}
+.right-recommend .right-recommend-head {
+  height: 50px;
+  line-height: 50px;
+  padding: 0 20px;
+  font-weight: 600;
+  border-bottom: 1px solid #f6f6f6;
+}
+.right-recommend .right-recommend-body {
+  padding: 16px 20px;
+}
+.right-recommend-body-item {
+  display: flex;
+  padding-bottom: 10px;
+  border-bottom: 1px solid #f6f6f6;
+}
+
+.right-recommend-body-item .right-recommend-body-img img {
+  width: 80px;
+  height: 64px;
+  border-radius: 3px;
+  margin-right: 8px;
+  object-fit: cover;
+}
+.right-recommend-body-inf {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.right-recommend-body-inf .right-recommend-body-top {
+  width: 165px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  margin-bottom: 5px;
+}
+.right-recommend-body-inf .right-recommend-body-bottom {
+  display: flex;
+  justify-content: space-between;
+}
+.right-recommend-body-bottom-person {
+  color: #8590a6;
+}
+.right-recommend-body-bottom-read >>> .ant-btn {
+  padding: 0px 6px;
+  height: auto;
+  color: #fff;
+  background: #0084ff;
+}
+.right-recommend-body-bottom-read >>> .ant-btn span{
+  margin-left: 0;
+}
+</style>
